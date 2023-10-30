8dc33de098991f86fdfc53a163de1a8548557683
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-10-30 14:01:38 +0100

    test download csv

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from 'react'
 import './main.css'
 
 function App() {
-  return <h1>App</h1>
+  const [csv, setCSV] = useState()
+
+  const data = [  
+    ['Alan Walker', 'Singer'],  
+    ['Cristiano Ronaldo', 'Footballer'],  
+    ['Saina Nehwal', 'Badminton Player'],  
+    ['Arijit Singh', 'Singer'],  
+    ['Terence Lewis', 'Dancer']  
+ ]
+
+  const download = () => {
+    console.log("download")
+
+    setCSV('Name,Profession\n')
+
+    for (const row of data) {  
+      setCSV(c => c + row.join(';'))
+      setCSV(c => c + "\n") 
+    }
+    
+    let hiddenCSV = document.createElement('a');  
+    hiddenCSV.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);  
+    hiddenCSV.target = '_blank';  
+    hiddenCSV.download = 'Programme.csv';  
+    hiddenCSV.click();  
+  }
+
+  return <>
+    {csv}
+    <button onClick={download}>Télécharger</button>
+  </>
 }
 
 export default App
